611145061e842b1c974456031400ab2a39995d82
mooracle <yanuar.prakosa@gmail.com>
2022-04-08 10:46:03 +0700

    inital commit (main and README)

---
+++ README.md
@@ -0,0 +1,3 @@
+# CS 50 GAMING: BREAKOUT
+
+This is part of the practical exercise on CS 50 gaming course. Here is the [source GitHub repo for this course](https://github.com/games50/breakout). And this is the [YouTube video link](https://www.youtube.com/watch?v=F86edI_EF3s&list=PLhQjrBD2T383Vx9-4vJYFsJbvZ_D17Qzh&index=4) to the course.
---
+++ main.lua
@@ -0,0 +1,10 @@
+--[[ 
+    CS 50 GAMING BREAKOUT
+
+    This is the main.lua which will be searched first by the compiler once the Love2D is run. 
+
+    In this source code most likely will only consist of includes syntax referring to other file locations. 
+
+    This in order to create programming paradigm inline wiht MVC sofware design pattern.
+ ]]
+ 
