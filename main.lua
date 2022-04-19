d7a54c3cdd34e21ab77bfbcd854d5a95b60f7713
mooracle <yanuar.prakosa@gmail.com>
2022-04-19 16:03:23 +0700

    input dependency to push and Class
    This two libs are ready to be inputted to the main.lua
    
    I think the dependency can only be used when it is already being made.
    Also the include must follow the right order of dependency

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
---[[ 
-    CS 50 GAMING BREAKOUT
+--[[ CS 50 GAMING BREAKOUT
 
     This is the main.lua which will be searched first by the compiler once the Love2D is run. 
 
@@ -7,4 +6,18 @@
 
     This in order to create programming paradigm inline wiht MVC sofware design pattern.
  ]]
- 
+
+-- dependencies:
+
+push = require 'lib/push' // https://github.com/Ulydev/push
+--[[ push 
+    push is a library that will allow us to draw our game at a virtual resolution, 
+    instead of however large our window is;
+    used to provide more retro aesthetic
+ ]]
+Class = require 'lib/class' // https://github.com/vrld/hump/blob/master/class.lua
+--[[ Class
+    Class library will allow us to represent anythin in our game as code,
+    rather tah keeping track of many disparate variable and methods.
+ ]]
+
