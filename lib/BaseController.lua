6a48721b281b7726bad365b21ef8203657b7325b
mooracle <yanuar.prakosa@gmail.com>
2022-05-09 16:15:23 +0700

    add BaseViewer and BaseController
    These are the interface which implementations will handle rendering and
    controlling of the UI and user interactions respectively.
    
    NOTE; this is still a basic design subject to revisions.

---
+++ lib/BaseController.lua
@@ -0,0 +1,9 @@
+--[[ 
+    Base Contorller will handle the update and user inputs on the program runs
+
+    The implementation of this class must be the 2nd argument to the BaseStateHandler implementation
+ ]]
+
+ BaseController = Class{}
+
+ function BaseController:update(dt)
---
+++ lib/BaseViewer.lua
@@ -0,0 +1,9 @@
+--[[ 
+    Base viewer will handle what is rendered into the the screen
+
+    the implementation of this class must be put as first argument to the BaseStateHandler implementation class
+ ]]
+
+ BaseViewer = Class{}
+
+ function BaseViewer:render() end
