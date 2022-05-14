cc145d6fd8eb62661d73271f93221bcdaf9cdae1
m00racle <yanuar.prakosa@gmail.com>
2022-05-15 06:15:07 +0700

    add change state for the state machine transition

--- lib/StateMachine.lua
+++ lib/StateMachine.lua
@@ -21,6 +21,13 @@ function StateMachine:init(states)
     self.current = self.empty
 end
 
+function StateMachine:change(stateName, enterParams)
+	assert(self.states[stateName]) -- state must exist!
+	self.current:exit()
+	self.current = self.states[stateName]()
+	self.current:enter(enterParams)
+end
+
 function StateMachine:update(dt)
     self.current:update(dt)
 end
