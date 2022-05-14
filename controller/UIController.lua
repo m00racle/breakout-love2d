0e1bbc48ea9bc62e579f7ec4329016edb8de4494
m00racle <yanuar.prakosa@gmail.com>
2022-05-15 06:30:13 +0700

    fix UIController to use gameStates:control(dt) rather than update(dt)
    In our code the render is view and the update is control(dt)

--- controller/UIController.lua
+++ controller/UIController.lua
@@ -4,7 +4,7 @@
 
  function love.update(dt)
     -- this time, we pass in dt to the state object we're currently using
-    gameStates:update(dt)
+    gameStates:control(dt)
 
     -- reset keys pressed
     love.keyboard.keysPressed = {}
