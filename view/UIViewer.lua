ae2df72bd2d56590e0877a65f73c3334b343ad30
mooracle <yanuar.prakosa@gmail.com>
2022-05-14 23:00:32 +0700

    put UIViewer for love.draw function
    This function is thw one calling the compiler to render the view
    
    TODO put the start state as first game state
    
    TODO make the FPS viewer function

---
+++ view/UIViewer.lua
@@ -0,0 +1,30 @@
+--[[ 
+    UIViewer is the file contains the functions useful to present the UI
+
+    love.draw()
+
+    displayFPS()
+ ]]
+
+function love.draw()
+    -- push the virtual resolution start
+    push:apply('start')
+
+    -- background will always be drawn regardless the state of the application
+    local backgroundwidth = gameTextures['background']:getWidth()
+    local backgroundHeight = gameTextures['background']:getHeight()
+
+    love.graphics.draw(gameTextures['background'],
+        -- draw start coordinate 0,0
+        0, 0,
+        -- no rotation
+        0,
+        -- scale factors on X and Y axis so it fills the screen
+        VIRTUAL_WIDTH/(backgroundwidth - 1), VIRTUAL_HEIGHT/(backgroundHeight - 1))
+    
+    -- TODO use the stete machine to defer rendering to the current state 
+
+    -- TODO display FPS for debugging -> comment out to reove later on
+
+    push:apply('end')
+end
