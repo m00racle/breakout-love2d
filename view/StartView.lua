7e41589ef5eb88998f337b5a60c483d507f014a5
mooracle <yanuar.prakosa@gmail.com>
2022-05-14 09:21:24 +0700

    prepare the view for the startview state
    This is the viewer class for the start state later on.
    
    This will be initialized in the startState class later on.

---
+++ view/StartView.lua
@@ -0,0 +1,42 @@
+--[[ 
+    StartView is a class of the start state views
+
+    This is where we will handle the start states rendered images
+ ]]
+
+ StartView = Class{__includes = BaseViewer}
+
+ -- I need to highlight the high score in this state
+ -- thus, I will make status in which it is highlighted 
+ local highlighted = 1
+
+ function StartView:render()
+    -- title
+    love.graphics.setFont(gFonts['large'])
+    love.graphics.printf("BREAKOUT", 0, VIRTUAL_HEIGHT / 3,
+        VIRTUAL_WIDTH, 'center')
+    
+    -- instructions
+    love.graphics.setFont(gFonts['medium'])
+
+    -- if we're highlighting 1, render that option blue
+    if highlighted == 1 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("START", 0, VIRTUAL_HEIGHT / 2 + 70,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+
+    -- render option 2 blue if we're highlighting that one
+    if highlighted == 2 then
+        love.graphics.setColor(103/255, 1, 1, 1)
+    end
+    love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 90,
+        VIRTUAL_WIDTH, 'center')
+
+    -- reset the color
+    love.graphics.setColor(1, 1, 1, 1)
+    
+ end
