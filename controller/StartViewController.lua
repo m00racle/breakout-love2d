55cb96f48beee8a82ac892a72e6083d29a8c1853
m00racle <yanuar.prakosa@gmail.com>
2022-05-15 06:25:14 +0700

    add StartViewController
    This is the controller class instance for Start gameStates

---
+++ controller/StartViewController.lua
@@ -0,0 +1,22 @@
+--[[ 
+    StartViewController
+
+    This is the class that handles controller for StartView
+    - update the view
+    - handle user inputs
+ ]]
+
+StartViewController = Class{__includes = BaseController}
+
+function StartViewController:update(dt)
+    -- toggle highlighted option if we press an arrow key up or down
+    if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+        highlighted = highlighted == 1 and 2 or 1
+        gameSounds['paddle-hit']:play()
+    end
+
+    -- we no longer have this globally, so include here
+    if love.keyboard.wasPressed('escape') then
+        love.event.quit()
+    end
+end
