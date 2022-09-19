7857275b028b2ecbef32978e25dd05119e6f96c8
m00racle <yanuar.prakosa@gmail.com>
2022-09-19 09:15:12 +0700

    give init and hit function on Brick
    TODO; make the brick QUADS and put it into
    gameFrames table to be rendered later by Brick class.

--- model/Brick.lua
+++ model/Brick.lua
@@ -1,26 +1,37 @@
 --[[
     Brick is a class representing Brick object in the game
+    When brick is hit the ball should bounce with determined angle depending on which part of the brick is hit
+    Then it should play sound.
+    Then it should drop tier or goes outplay when tier is already 0
 ]]
 
 Brick = Class{}
 
 function Brick:init(x,y)
-    -- TODO: put each variables thier initial values. 
+    -- : put each variables thier initial values. 
     self.x = x
     self.y = y
-    -- self.tier = 1
-    -- self.color = 1
-    -- self.width = int
-    -- self.height = int
+    self.tier = 0
+    self.color = 1
+    self.width = 32
+    self.height = 16
     self.inPlay = true
 end
 
 function Brick:hit()
-    -- TODO: what are need to do when the brick is hit by the ball
+    -- : what are need to do when the brick is hit by the ball
     -- Play sound
+    gameSounds['brick-hit-2']:play()
+
+    -- TEST: put the brick out of play
+    self.inPlay = false
 end
 
 function Brick:render()
     -- TODO: what to render
-    
+    -- if brick is still inPlay then 
+    if self.inPlay then
+        -- render the brick quad that is taken from ... gameFrames
+        -- TODO: PUT THE quads for all brick types into gameFrames
+    end
 end
