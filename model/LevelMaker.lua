8e3cc824329783c988c8a587a1866db4fa19cbf7
m00racle <yanuar.prakosa@gmail.com>
2022-09-24 07:14:33 +0700

    fix the LevelMaker.lua to show full extend bricks
    The main error lies in the formula to lay down all bricks. Some parts should be additions instead of multiplications.

--- model/LevelMaker.lua
+++ model/LevelMaker.lua
@@ -22,10 +22,14 @@ This will decide the difficulties levels of the brick structure.
     local bricks = {}
 
     -- random choode the number of rows between 1 to 5
-    local numRows = math.random(1,5)
+    -- local numRows = math.random(1,5)
+    -- TEST: numRow constant
+    local numRows = 5
 
     -- randomly choose number of columns (from 7 to 13)
-    local numCols = math.random(7, 13)
+    -- local numCols = math.random(7, 13)
+    -- TEST: numCol constant
+    local numCols = 13
 
     -- lay out the bricks such that they touch each other and fill the space 
     for y = 1, numRows do 
@@ -34,8 +38,8 @@ This will decide the difficulties levels of the brick structure.
                 -- x coordinate
                 (x-1)                    --decrement x by 1 because tables are 1-indexed, coords are 0
                 * 32                     -- multiply by 32 pixels of brick's width
-                * 8                      -- the screen should have 8 pixels of padding; we can fit 13 cols + 16 pixels total
-                *(13 - numCols) * 16,    -- left-side padding for when there are fewer than 13 columns
+                + 8                      -- the screen should have 8 pixels of padding; we can fit 13 cols + 16 pixels total
+                + (13 - numCols) * 16,    -- left-side padding for when there are fewer than 13 columns
                 -- y coordinates
                 y * 16                  -- just use y * 16, since we need top padding anyway
             )
