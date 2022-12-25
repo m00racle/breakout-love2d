688071b6db1777a21feedd92e33630d6517d7cf7
mooracle <yanuar.prakosa@gmail.com>
2022-12-26 06:42:04 +0700

    PASS: brick update to change tier and color on hit
    This will differentiate the brick strength based on tier and color.
    
    The tier will only deprecated once the color reach base color=1
    
    There are 4 tiers 5 colors as level

--- model/Brick.lua
+++ model/Brick.lua
@@ -21,10 +21,35 @@ end
 function Brick:hit()
     -- : what are need to do when the brick is hit by the ball
     -- Play sound
+    gameSounds['brick-hit-2']:stop()
+    -- NOTE: this to make sure if multiple bricks is hit the sound will not overlap
     gameSounds['brick-hit-2']:play()
 
-    -- TEST: put the brick out of play
-    self.inPlay = false
+    -- if tier is > 0 then look at color!:
+    -- IF color == 1 (lowest color) then degrade one tier ELSE degrade one color
+    -- ELSE (if tier == 0) then IF color == 1 bricks out of play ELSE (color > 1) degrade color
+    if self.tier > 0 then 
+        if self.color == 1 then
+            self.tier = self.tier - 1
+            self.color = 5 
+            -- NOTE: when tier is degraded then color is back to max which is 5
+        else 
+            self.color = self.color -1
+        end
+    else
+        if self.color == 1 then
+            self.inPlay = false
+        else
+            self.color = self.color - 1
+        end
+    end
+
+    -- put different sond when bricks is OUT OF PLAY
+    if not self.inPlay then
+        gameSounds['brick-hit-1']:stop()
+        gameSounds['brick-hit-1']:play()
+    end
+    -- end of Brick:hit() function
 end
 
 function Brick:setColor(color)
